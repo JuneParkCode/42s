41a256e32354ef76c229f93a793fb5cd44535a7e
June <khsj15@khu.ac.kr>
2022-02-14 22:51:02 +0900

    split solved

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -1,6 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 int	ft_is_split(char c, char *split)
 {
-	while (*split != c)
+	while (*split != c && *split)
 		++split;
 	return (*split == c);
 }
@@ -16,13 +19,14 @@ int	ft_get_word_num(char *str, char *charset)
 	flag = 1;
 	while (str[i])
 	{
-		if (ft_is_split(str[i]) && !flag)
+		if (ft_is_split(str[i], charset) && !flag)
 		{
 			num++;
 			flag = 1;
 		}
 		else
 			flag = 0;
+		++i;
 	}
 	return (num + 1);
 }
@@ -37,28 +41,44 @@ void	ft_strncpy(char *s1, char *s2, int n)
 		s1[i] = s2[i];
 		++i;
 	}
-	while (i < n)
-		s1[i++] = 0;
+	s1[n] = 0;
+}
+
+char	*ft_get_word(char *str, char *charset, int *start)
+{
+	int		idx_str;
+	char	*word;
+
+	idx_str = 0;
+	while (!ft_is_split(str[idx_str], charset))
+		++(idx_str);
+	if (idx_str == 0)
+		return (0);
+	word = (char *) malloc(sizeof(char) * (idx_str + 1));
+	ft_strncpy(word ,str, idx_str);
+	*start += idx_str;
+	return (word);
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	char	*word;
 	char	**result;
-	int		len_word;
+	int		idx_str;
 	int		num_word;
-	int		idx;
+	int		idx_res;
 
 	num_word = ft_get_word_num(str, charset);
 	result = (char **) malloc(sizeof(char *) * (num_word + 1));
-	idx = 0;
-	while (*str)
+	idx_str = 0;
+	idx_res = 0;
+	while (str[idx_str])
 	{
-		if (ft_is_split(*str))
-		{
-			word = (char *) malloc(sizeof(char) * (len_word + 1));
-			ft_strncpy(word, original, len_word);
-			result[idx++]
-		}
+		word = ft_get_word(&str[idx_str], charset, &idx_str);
+		if (word)
+			result[idx_res++] = word;
+		++idx_str;
 	}
+	result[idx_res] = 0;
+	return (result);
 }
