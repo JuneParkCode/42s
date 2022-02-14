bb540d97309acfad47a50d46d296b2ca8988358c
June <khsj15@khu.ac.kr>
2022-02-14 22:51:02 +0900

    split solved

--- c07/ex05/test.c
+++ c07/ex05/test.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	main(void)
+char	**ft_split(char *str, char *charset);
+
+void do_test(char* str, char* charset)
 {
-	char	*str = "abcd";
-	char	*str2 = str;
+	char	**array;
+	int		i;
 
-	str2++;
-	printf("%d", (str2 - str));
-	return (0);
+	array = ft_split(str, charset);
+	printf("%p\n",array[i]);
+	i = 0;
+	while (array[i])
+	{
+		printf("'%s' (len=%d)\n", array[i], (int)strlen(array[i]));
+		i++;
+	}
+}
+
+int main(void)
+{
+	do_test(",1,2,3", ",");
+	do_test("Hello.,World,.!!KOKO!ZZZ.Hello.Good.World!KK!ZORO,Good", ",.!");
+	do_test("abcakaabcaakaabce", "bck");
+	do_test("|---AA-|GoogooGooGoo|aaaa-| Hello World Good| ^^|Nice Boat!", " |-");
+	do_test("               ", "     ");
+	do_test("  \t ", " \t");
+	do_test("123,456,789 ,", ",l1");
+	do_test("0tNue8", "0tNue8");
+	do_test("80hzNIGZYoIa3ATwY8dRCFmBBYx0RA", "AKfwjE5l");
+	return 0;
 }
