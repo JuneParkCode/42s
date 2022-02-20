c7dbda13bc1b38fd77db22def139a22741263953
June <khsj15@khu.ac.kr>
2022-02-20 22:13:25 +0900

    hexdump : no option complete"

---
+++ c10/ex03/ft_hexdump.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hexdump.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 20:48:07 by sungjpar          #+#    #+#             */
+/*   Updated: 2022/02/20 22:11:21 by sungjpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_msg(char *file_name, int err_code);
+void	print_line(char *buf_current, char *buf_prev, int *is_first, long addr);
+
+void	hexdump(char *file_name, char *buf_current, char *buf_prev)
+{
+	int	fd;
+	int	read_size;
+	int	residue;
+	int	flag;
+	int	addr;
+
+	fd = open(file_name, O_RDONLY);
+	flag = -1;
+	addr = 0;
+	if (fd == -1)
+	{
+		ft_msg(file_name, errno);
+		return ;
+	}
+	residue = 0;
+	read_size = read(fd, buf_current, 16);
+	while (read_size > 0)
+	{
+		if ((16 - read_size) != 0)
+			residue = 16 - read_size;
+		else
+		{
+			residue = 16;
+			print_line(buf_current, buf_prev, &flag, addr);
+			addr += 16;
+		}
+		read_size = read(fd, &buf_current[16 - residue], residue);
+	}
+	addr += 16 - residue;
+	// 남은 buffer 출력 및 hex_address 출력
+	close(fd);
+}
+
+void	hexdump_c(char *file_name, char *buf_current, char *buf_prev)
+{
+}
+
+void	hexdump_stdin(char *buf_current, char *buf_prev)
+{
+}
+
--- c10/ex03/ft_main.c
+++ c10/ex03/ft_main.c
@@ -6,11 +6,46 @@
 /*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:25:21 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/02/20 14:25:46 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/02/20 21:37:59 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define BUFFER_SIZE 16
+
+void	hexdump(char *file_name, char *buf_current, char *buf_prev);
+void	hexdump_c(char *file_name, char *buf_current, char *buf_prev);
+void	hexdump_stdin(char *buf_current, char *buf_prev);
+
 int	main(int argc, char *argv[])
 {
+	char	buf_current[16];
+	char	buf_prev[16];
+	int		idx;
+
+	hexdump(argv[1], buf_current, buf_prev);
+/*	
+	if (argc == 1)
+	{
+		hexdump_stdin(buf_current, buf_prev);
+		return (0);
+	}
+	if (strcmp(argv[1], "-C") == 0)
+	{
+		idx = 2;
+		if (argc == 2)
+		{
+			hexdump_c_stdin(buf_current, buf_prev);
+			return (0);
+		}
+		while (idx < argc)
+			hexdump_c(argv[idx++], buf_current, buf_prev);
+	}
+	else
+	{
+		idx = 1;
+		while (idx < argc)
+			hexdump(argv[idx++], buf_current, buf_prev);
+	}
+*/
 	return (0);
 }
