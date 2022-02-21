0fff54548490595ce17e427b1e69baf724fe60f9
Sungjun Park <sungjpar@c7r7s2.42seoul.kr>
2022-02-21 20:29:31 +0900

    c11

---
+++ c11/ex03/ft_count_if.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 18:38:20 by sungjpar          #+#    #+#             */
+/*   Updated: 2022/02/21 18:40:49 by sungjpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_count_if(char **tab, int length, int(*f)(char*))
+{
+	int	count;
+	int	idx_tab;
+
+	count = 0;
+	idx_tab = 0;
+	while (idx < length)
+		count += f(tab[idx++]) != 0
+}
