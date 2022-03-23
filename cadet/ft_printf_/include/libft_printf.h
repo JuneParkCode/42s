40249e679f8b713ab2061d247816de76de6e5702
June <khsj15@khu.ac.kr>
2022-03-23 22:29:24 +0900

    ft_libft : test done

--- cadet/ft_printf_/include/libft_printf.h
+++ cadet/ft_printf_/include/libft_printf.h
@@ -6,20 +6,21 @@
 /*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:10:31 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/03/23 16:08:19 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/03/23 22:27:39 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_PRINTF_H
 # define LIBFT_PRINTF_H
-
+# define ABS(X) X < 0 ? -X : X
 # include <stddef.h>
 # include <stdarg.h>
 int		ft_printf(const char *msg, ...);
 int		ft_putchar(const char c);
 int		ft_putstr(const char *str);
-int		ft_putnbr_base(const int n, const char *base);
+int		ft_putnbr(const long long n);
 int		ft_putaddr(void *addr);
+int		ft_puthex(unsigned int n, int flag);
 size_t	ft_strlen(const char *str);
 
 #endif
--- cadet/ft_printf_/src/ft_putaddr.c
+++ cadet/ft_printf_/src/ft_putaddr.c
@@ -6,29 +6,37 @@
 /*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 14:41:15 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/03/23 15:32:42 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/03/23 21:39:17 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-static void	print_hex(unsigned long long n, int length, int depth)
+#include <unistd.h>
+#include "../include/libft_printf.h"
+
+static int	ft_putaddr_rec(size_t n, int length, int depth)
 {
 	unsigned char	c;
 	char			*hex;
 	int				i;
-
+	
 	hex = "0123456789abcdef";
 	if (n < 0)
 		n = -n;
 	i = 0;
 	c = hex[n % 16];
 	if (depth < length)
-		print_hex(n / 16, length, depth + 1);
+		ft_putaddr_rec(n / 16, length, depth + 1);
 	write(1, &c, 1);
+	return (16);
 }
 
 int	ft_putaddr(void *addr)
 {
-	print_hex((unsigned long long) addr, 16, 1);
-	return (16);
+	if (addr == 0)
+	{
+		ft_putchar('0');
+		return (1);
+	}
+	ft_putaddr_rec((intptr_t) addr, 12, 1);
+	return (12);
 }
