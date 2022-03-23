054d1fef23a687dde83bf934de48669b434db4eb
June <khsj15@khu.ac.kr>
2022-03-23 19:12:14 +0900

    mv

---
+++ cadet/ft_printf_/src/ft_strlen.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungjpar <sungjpar@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 18:28:01 by sungjpar          #+#    #+#             */
+/*   Updated: 2022/03/23 16:09:45 by sungjpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+/* Function 	: 	ft_strlen
+ * Description	:	computes length of string s
+ * Param		:
+ * 			s	:	address of first character of string to test
+ * return value	:	characters that precede the terminating NUL charcter
+ */
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	length = 0;
+	while (s[length])
+		length++;
+	return (length);
+}
