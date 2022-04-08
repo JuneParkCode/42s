590dad2899251242d51932db7414ea8a31bd75e6
Sungjun Park <sungjpar@c1r4s7.42seoul.kr>
2022-04-08 15:08:24 +0900

    gnl : sol in open_max

--- cadet/get_next_line/t2/test.c
+++ cadet/get_next_line/t2/test.c
@@ -6,7 +6,14 @@
 
 int	main(void)
 {
-	int fd = open("./files/empty", O_RDONLY);
-	printf("%s", get_next_line(fd));
+	int 	fd = open("./files/multiple_nlx5", O_RDONLY);
+	char	*s;
+	int 	i = 0;
+	while (s = get_next_line(fd))
+	{
+		printf("%d : %s", ++i, s);
+		if (s)
+			free(s);
+	}
 	return (0);
 }
