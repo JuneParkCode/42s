1448411f5246433ce6f40662e2e78fd90f1c232e
Sungjun Park <sungjpar@c5r7s5.42seoul.kr>
2022-04-15 15:05:29 +0900

    add : set_print_color

--- cadet/libft/src/ft_set_print_color.c
+++ cadet/libft/src/ft_set_print_color.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_set_print_color.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 15:10:31 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/04/11 16:06:46 by sungjpar         ###   ########.fr       */
+/*   Created: 2022/04/15 14:52:11 by sungjpar          #+#    #+#             */
+/*   Updated: 2022/04/15 15:04:03 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include <stddef.h>
-# include <stdarg.h>
-# include "libft.h"
-int		ft_printf(const char *msg, ...);
-#endif
+#include "libft.h"
+#include "ft_set_print_color.h"
+
+void	ft_set_print_color(const char *color)
+{
+	ft_printf(color);
+}
