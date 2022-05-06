f88d395ef553702d1ffea03d564c78c774cfdcdc
June <khsj15@khu.ac.kr>
2022-05-06 19:54:29 +0900

    relocation file directory and make echo program

---
+++ cadet/myproject/myProgram/echo/include/echo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 19:35:52 by sungjpar          #+#    #+#             */
+/*   Updated: 2022/05/06 19:38:46 by sungjpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ECHO_H
+# define ECHO_H
+
+# include "libft.h"
+
+# define SUCCESS 0
+# define FAILED -1
+
+int	echo(int argc, char *argv[]);
+
+#endif
