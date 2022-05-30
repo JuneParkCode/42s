8ff0356d3aa6593854f676d4444f19ac3739d197
June <khsj15@khu.ac.kr>
2022-05-30 21:21:03 +0900

    0530

--- cadet/myproject/data_structures_c/bst/src/bst_delete_item.c
+++ cadet/myproject/data_structures_c/bst/src/bst_delete_item.c
@@ -6,13 +6,22 @@
 /*   By: sungjpar <sungjpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 20:35:24 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/05/30 10:29:57 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/05/30 11:10:17 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "bst.h"
 
+/*
+	Delete cases
+	case #1
+		: left, right -> empty
+	case #2
+		: only one (left or right) node is empty
+	case #3
+		: left and right node is exists
+*/
 void	bst_delete_item(t_bst *tree, void *item)
 {
 	t_bst_node	*node;
@@ -20,4 +29,5 @@ void	bst_delete_item(t_bst *tree, void *item)
 	node = find_node(tree, item);
 	if (node == NULL)
 		return ;
+	// 나중에 짜죠
 }
