6a5b055ed0276f7eb2f42a42b3db35fc99e79711
June <khsj15@khu.ac.kr>
2022-06-11 21:00:06 +0900

    prototype : graph

---
+++ cadet/myproject/data_structures_cpp/graph/graph.hpp
@@ -0,0 +1,25 @@
+#ifndef __GRAPH_HPP__
+#define __GRAPH_HPP__
+
+template<class ItemType>
+class graph
+{
+private:
+
+public:
+	graph();
+	~graph();
+};
+
+template<class ItemType>
+graph<ItemType>::graph()
+{
+}
+
+template<class ItemType>
+graph<ItemType>::~graph()
+{
+}
+
+
+#endif
