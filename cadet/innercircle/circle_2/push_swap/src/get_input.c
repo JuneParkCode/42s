f165225a8a3cf130b3343a84e068454c49c93e61
June <khsj15@khu.ac.kr>
2022-07-06 09:43:16 +0900

    fix : push_swap mem leaks

--- cadet/innercircle/circle_2/push_swap/src/get_input.c
+++ cadet/innercircle/circle_2/push_swap/src/get_input.c
@@ -6,7 +6,7 @@
 /*   By: sungjpar <sungjpar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 20:57:29 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/07/04 17:05:06 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/07/06 09:38:32 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 #include "libft.h"
 #include "push_swap.h"
 
-static void	free_splited_string(char **strs)
+static void	free_splited_string(char **splited_string)
 {
 	int	idx;
 
 	idx = 0;
-	while (strs != NULL && strs[idx] != NULL)
-		free(strs[idx++]);
-	if (strs != NULL)
-		free(strs);
+	while (splited_string != NULL && splited_string[idx] != NULL)
+		free(splited_string[idx++]);
+	if (splited_string != NULL)
+		free(splited_string);
 }
 
 t_status	get_input(const int argc, char **argv, t_deque *dq)
 {
-	char	**strs;
+	char	**splited_string;
 	int		arg;
 
 	arg = 1;
@@ -35,14 +35,17 @@ t_status	get_input(const int argc, char **argv, t_deque *dq)
 	{
 		if (ft_strlen(argv[arg]) == 0)
 			return (FAILED);
-		strs = ft_split(argv[arg], ' ');
-		if (is_valid_string(strs) == TRUE)
+		splited_string = ft_split(argv[arg], ' ');
+		if (is_valid_string(splited_string) == TRUE)
 		{
-			push_strings_to_deque(strs, dq);
-			free_splited_string(strs);
+			push_strings_to_deque(splited_string, dq);
+			free_splited_string(splited_string);
 		}
 		else
+		{
+			free_splited_string(splited_string);
 			return (FAILED);
+		}
 		++arg;
 	}
 	return (SUCCESS);
