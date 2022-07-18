e191ac707c1bbe7f6462bd7f1fb547b9b8703d2c
June <khsj15@khu.ac.kr>
2022-07-18 17:46:31 +0900

    [libft] fix : strtrim length 2 case

--- cadet/innercircle/circle_0/libft/src/ft_strcspn.c
+++ cadet/innercircle/circle_0/libft/src/ft_strcspn.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strspn.c                                        :+:      :+:    :+:   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sungjpar <sungjpar@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 18:35:53 by sungjpar          #+#    #+#             */
-/*   Updated: 2022/04/11 15:46:19 by sungjpar         ###   ########.fr       */
+/*   Updated: 2022/07/18 16:40:57 by sungjpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ size_t	ft_strcspn(const char *str, const char *set)
 	idx_str = 0;
 	while (str[idx_str])
 	{
-		if (ft_strchr(set, str[idx_str]))
+		if (ft_isinset(str[idx_str], set))
 			return (idx_str);
+		++idx_str;
 	}
 	return (idx_str);
 }
